643ffd62b7473ac206025e12866fe38ea87bc16b
jallen19 <jallen@stcharlessd.org>
2017-11-01 12:09:18 -0500

    Converted from Farenheit to Celsius

--- Lab 01 - Calculator/lab_01_part_a.py
+++ Lab 01 - Calculator/lab_01_part_a.py
@@ -1,1 +1,7 @@
-#
+#!/usr/env python3
+#Lab 1.1 a
+#James Allen
+#11/01/17
+
+Temperature = int(input('Enter a temperature in Fahrenheit: '))
+print('The temperature in Celsius:', (Temperature - 32)/1.8)
