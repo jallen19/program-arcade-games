d9cb5504c09e9b041f9edf5d9848e72f27575b1e
jallen19 <jallen@stcharlessd.org>
2017-11-01 12:28:28 -0500

    Can calculate area of a trapezoid

--- Lab 01 - Calculator/lab_01_part_b.py
+++ Lab 01 - Calculator/lab_01_part_b.py
@@ -1,1 +1,9 @@
-#
+#!/usr/bin/env python#
+#Lab 1.2 part b
+#James Allen
+#11/1/17
+
+Height = int(input('Enter the height of the trapezoid: '))
+Bottom = int(input('Enter the length of the bottom base: '))
+Top = int(input('Enter the length of the top base: '))
+print('The area is:', (1/2)*(Top + Bottom)*Height)
