9f5d40e49d2d7fd7c61684f436ffa4a5cb2028a3
jallen19 <jallen@stcharlessd.org>
2017-11-01 12:43:22 -0500

    Calculate area of an equillateral triangle

--- Lab 01 - Calculator/lab_01_part_c.py
+++ Lab 01 - Calculator/lab_01_part_c.py
@@ -1,1 +1,8 @@
-#
+#!/usr/bin/env python3
+#Lab 1.3 c
+#James Allen
+#11/1/17
+
+print('Area of an equillateral triangle')
+Height = int(input('Enter the height: '))
+print((Height**2)*(1/3)**(1/2)/3)
